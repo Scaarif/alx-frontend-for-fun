623100f1750c64e30c2f514219ea727c91667da3
scaarifintl <scaarifintl@gmail.com>
2023-05-04 19:52:10 +0300

    t-0

---
+++ markdown2html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Takes as argument 2 strings, the name of Markdown file
+    and the output file name respectively.
+    Requirements:
+        - if the number of args < 2 print to STDERR
+        'Usage: ./markdown2html.py README.md README.html' and exit 1
+        - if the Markdown file doesnt exist print in STDERR
+        'Missing <filename>' and exit 1
+        - otherwise, print nothing and exit 0
+"""
+import sys
+import os
+
+
+if __name__ == '__main__':
+    # check the number of args
+    if len(sys.argv) < 3:
+        print('Usage: {} README.md README.html'.format(sys.argv[0]))
+        exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]))
+        exit(1)
+    print()
+    exit(0)
