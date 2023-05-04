52323e7a191cda47722766b17a890492cb1f15f2
scaarifintl <scaarifintl@gmail.com>
2023-05-04 20:18:49 +0300

    print to stderr

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,7 @@
     Requirements:
         - if the number of args < 2 print to STDERR
         'Usage: ./markdown2html.py README.md README.html' and exit 1
-        - if the Markdown file doesnt exist print in STDERR
+        - if the Markdown file doesn't exist print in STDERR
         'Missing <filename>' and exit 1
         - otherwise, print nothing and exit 0
 """
@@ -15,10 +15,10 @@ import os
 if __name__ == '__main__':
     # check the number of args
     if len(sys.argv) < 3:
-        print('Usage: {} README.md README.html'.format(sys.argv[0]))
+        print('Usage: {} README.md README.html'.format(
+            sys.argv[0]), file=sys.stderr)
         exit(1)
     if not os.path.exists(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]))
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
-    print()
     exit(0)
